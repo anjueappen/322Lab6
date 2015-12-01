48c65774c59fab6be6c75e7a28b1ab23a765920a
Anju <aeappen@e5-05-02.ece.ualberta.ca>
2015-12-01 16:49:58 -0700

    ArrayLib Tests

--- Lab6_Maths/tests/MathPackageTest.java
+++ Lab6_Maths/tests/MathPackageTest.java
@@ -2,6 +2,8 @@ package tests;
 
 import static org.junit.Assert.*;
 
+import org.apache.commons.math3.stat.StatUtils;
+import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
 import org.junit.Test;
 
 import src.MathPackage;
@@ -10,11 +12,159 @@ public class MathPackageTest {
 
 	@Test
 	public void testRandomNormal() {
-		MathPackage.random(3, 2, 7);
+		double[] returned = MathPackage.random(3, 2, 7);
+		for(double d: returned){
+			assertTrue(d >= 2);
+			assertTrue(d <= 7);
+		}
 	}
 	@Test
-	public void testRandomWith() {
-		return; 
+	public void testRandomInvertedRange() {
+		double[] returned = MathPackage.random(3, 7, 2);
+	}
+	
+	@Test
+	public void testMax() {
+		double [] array = new double[]{5,6,3,4,2,9};
+		double max = MathPackage.max(array); 
+		assertTrue(max == 9);
+		
+	}
+	@Test
+	public void testMaxAllSame() {
+		double [] array = new double[]{9,9,9};
+		double max = MathPackage.max(array); 
+		assertTrue(max == 9);
+	}
+	
+	@Test
+	public void testMaxNegative() {
+		double [] array = new double[]{-9,-8,-7};
+		double max = MathPackage.max(array); 
+		assertTrue(max == -7);
 	}
 
+	@Test
+	public void testMin() {
+		double [] array = new double[]{5,6,3,4,2,9};
+		double min = MathPackage.min(array); 
+		assertTrue(min == 2);
+	}
+	@Test
+	public void testMinAllSame() {
+		double [] array = new double[]{2,2,2};
+		double min = MathPackage.min(array); 
+		assertTrue(min == 2);
+		
+	}
+	
+	@Test
+	public void testMinNegative() {
+		double [] array = new double[]{-9,-8,-7};
+		double max = MathPackage.max(array); 
+		assertTrue(max == -9);
+	}
+	
+	@Test
+	public void testNormalize(){
+		double[] a = new double[]{5,6,3,4,2,9};
+		double [] array = MathPackage.normalize(a);
+		assertArrayEquals(array, StatUtils.normalize(a), 0.5);
+		
+	}
+	@Test
+	public void testNormalizeAllSame(){
+		double[] a = new double[]{5,5,5,5};
+		double [] array = MathPackage.normalize(a);
+		assertArrayEquals(array, StatUtils.normalize(a), 0.5);
+	}
+	
+	@Test
+	public void testNormalizeAllNegative(){
+		double[] a = new double[]{-5,-5,-5,-5};
+		double [] array = MathPackage.normalize(a);
+		assertArrayEquals(array, StatUtils.normalize(a), 0.5);
+	}
+	
+	@Test
+	public void testSum(){
+		double[] a = new double[]{5,6,3,4,2,9};
+		double sum = MathPackage.sum(a);
+		assertTrue(sum == StatUtils.sum(a));
+	}
+	
+	@Test
+	public void testSumZeros(){
+		double[] a = new double[]{0,0,0,0};
+		double sum = MathPackage.sum(a);
+		assertTrue(sum == StatUtils.sum(a));
+	}
+	
+	@Test
+	public void testSumNegatives(){
+		double[] a = new double[]{-5,-5,-5,-5};
+		double sum = MathPackage.sum(a);
+		assertTrue(sum == StatUtils.sum(a));
+	}
+
+	@Test
+	public void testStdDevAllSame(){
+		double[] a = new double[]{5,5,5,5};
+		double sum = MathPackage.stddev(a);
+		DescriptiveStatistics d = new DescriptiveStatistics(a); 
+		assertTrue(sum == d.getStandardDeviation());
+	}
+	
+	@Test
+	public void testStdDev(){
+		double[] a = new double[]{5,6,3,4,2,9};
+		double sum = MathPackage.stddev(a);
+		DescriptiveStatistics d = new DescriptiveStatistics(a); 
+		assertTrue(sum == d.getStandardDeviation());
+	}
+	
+	@Test
+	public void testStdDevNegatives(){
+		double[] a = new double[]{-5,-5,-5,-5};
+		double sum = MathPackage.stddev(a);
+		DescriptiveStatistics d = new DescriptiveStatistics(a); 
+		assertTrue(sum == d.getStandardDeviation());
+	}
+	@Test
+	public void testArrayAdd(){
+		double[] a = new double[]{-5,-5,-5,-5};
+		double[] b = new double[]{-5,-5,-5,-5};
+		double[] result = MathPackage.arrayAdd(a, b); 
+		for(int i = 0; i < a.length; i++){
+			assertTrue(result[i]  == a[i] + b[i]);
+		}
+	}
+	@Test
+	public void testArrayAddDiffLengths(){
+		double[] a = new double[]{-5,-5,-5,-5};
+		double[] b = new double[]{-5,-5,-5};
+		double[] result = MathPackage.arrayAdd(a, b); 
+		//Should fail here 
+		for(int i = 0; i < a.length; i++){
+			assertTrue(result[i]  == a[i] + b[i]);
+		}
+	}
+	
+	@Test
+	public void testArrayNegate(){
+		double[] a = new double[]{-5,-5,-5,-5};
+		double[] result = MathPackage.arrayNegate(a); 
+		for(double d: result){
+			assertTrue(d == 5);
+		}
+	}
+	
+	@Test
+	public void testArrayNegateZeros(){
+		double[] a = new double[]{0,0,0,0};
+		double[] result = MathPackage.arrayNegate(a); 
+		for(double d: result){
+			assertTrue(d == 5);
+		}
+	}
 }
